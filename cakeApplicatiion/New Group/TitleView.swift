985f04b5feda2a681661f42226f7c43bdfc2fe70
satyaarjun <arjunchowdary823@gmail.com>
2021-12-23 21:23:09 -0500

    titleview change

---
+++ cakeApplicatiion/New Group/TitleView.swift
@@ -0,0 +1,30 @@
+//
+//  TitleView.swift
+//  cakeApplicatiion
+//
+//  Created by arjun on 2021-12-23.
+//
+
+import Foundation
+import SwiftUI
+
+struct TitleView: View {
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color("PrimaryColor"))
+            
+            Spacer( )
+            
+        } // :HStack
+        .padding(.horizontal)
+        .padding(.top,15)
+        .padding(.bottom,10)
+    }
+}
+
+
