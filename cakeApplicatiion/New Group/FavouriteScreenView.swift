975a918e96deaf77cd1cde53034d3da983fd9ba0
Anmoldeepsingh0001 <74370241+Anmoldeepsingh0001@users.noreply.github.com>
2022-01-10 19:27:05 -0500

    Added Favourite Screen View

--- cakeApplicatiion/New Group/FavouriteScreenView.swift
+++ cakeApplicatiion/New Group/FavouriteScreenView.swift
@@ -1,15 +1,28 @@
 //
 //  FavouriteScreenView.swift
-//  cakeApplicatiion
+//  cakeApplication
 //
-//  Created by Kinjal Kunjadiya on 2021-12-13.
+//  Created by Anmol
 //
 
 import SwiftUI
 
 struct FavouriteScreenView: View {
+    let cakes = Cake.getAllCakes( )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let chunkedCakes = cakes.chunked(2)
+            return List {
+//                ForEach(0 ..< chunkedCakes.count) { index in
+                ForEach(0 ..< 3) { index in
+                    HStack{
+                        ForEach( chunkedCakes[index]) { cake in
+                            CakeFavouriteCellView(cake: cake)
+                        }
+                    }
+                }
+                .padding(.top, 5)
+        }
     }
 }
 
@@ -18,3 +31,38 @@ struct FavouriteScreenView_Previews: PreviewProvider {
         FavouriteScreenView()
     }
 }
+
+struct CakeFavouriteCellView: View {
+    
+    let cake: Cake
+    @State var showModel = false
+    
+    var body: some View {
+        
+        VStack(alignment:  .leading) {
+            Image(cake.image)
+                .resizable( )
+                .frame(height: 190)
+                .padding(.horizontal,5)
+            
+            VStack(alignment:  .leading) {
+                HStack {
+                Text(cake.name)
+                    .font(.headline.bold( ))
+                    .foregroundColor(Color("PrimaryColor"))
+                
+                Image(systemName: "star.fill")
+                        .foregroundColor(Color("PrimaryColor"))
+                        .font(.title3)
+                }
+            }
+            .padding([.leading,  .bottom], 10)
+            .padding(.vertical, 15)
+            .padding(.top, 5)
+        }
+        .cornerRadius(20)
+        .shadow(color: Color.gray.opacity(0.8) ,radius: 15)
+
+    }
+}
+
