6ac0c51e0efbd2d41dcdcda84522f6798946e226
arjun3333 <arjunchowdary823@gmail.com>
2022-01-25 23:44:05 -0500

    cake info

--- CakeApp/Scenes/ViewControllers/CakeInfoViewController.swift
+++ CakeApp/Scenes/ViewControllers/CakeInfoViewController.swift
@@ -1,1 +1,30 @@
 
+//
+//  CakeInfoViewController.swift
+//  CakeApp
+//
+//  Created by arjun on 18/01/22.
+//
+
+import UIKit
+
+class CakeInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
