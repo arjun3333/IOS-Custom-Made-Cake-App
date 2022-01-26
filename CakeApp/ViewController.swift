84208fe7755c6ea0e1e6c4f8d9128120baa0f146
arjun3333 <arjunchowdary823@gmail.com>
2022-01-25 23:45:23 -0500

    view

--- CakeApp/ViewController.swift
+++ CakeApp/ViewController.swift
@@ -1,1 +1,32 @@
 
+//
+//  ViewController.swift
+//  CakeApp
+//
+//  Created by arjun on 08/01/22.
+//
+
+import UIKit
+
+
+
+
+class ViewController: UIViewController {
+
+    //MARK:- Outlets
+    @IBOutlet weak var btnWelcome: UIButton!
+    
+    @IBAction func btnWelcomeClick(_ sender: Any) {
+        UIApplication.shared.setHome()
+    }
+    
+    //MARK:- View LifeCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        DispatchQueue.main.async {
+            self.btnWelcome.layer.cornerRadius = self.btnWelcome.frame.height / 2
+        }
+    }
+}
+
