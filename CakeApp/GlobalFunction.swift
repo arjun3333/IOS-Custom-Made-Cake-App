a49a3139e4acc87281126beae833c92fe74b78aa
kinjal-14 <kunjadiyakinjal147@gmail.com>
2022-01-26 00:00:32 -0500

    Create Global Function

--- CakeApp/GlobalFunction.swift
+++ CakeApp/GlobalFunction.swift
@@ -1,1 +1,38 @@
+//
+//  GlobalFunction.swift
+//  CarRental
+//
+//  Created by 2022M3 on 19/01/22.
+//
+
+import Foundation
+
+
+
+class GFunction {
+    
+    static let shared: GFunction = GFunction()
+    static var user = UserModel()
+    
+    func firebaseLogin(data: String) {
+        FirebaseAuth.Auth.auth().signIn(withEmail: data, password: "123123") { [weak self] authResult, error in
+            guard self != nil else { return }
+            //return if any error find
+            if error != nil {
+                FirebaseAuth.Auth.auth().createUser(withEmail: data, password: "123123") { authResult, error in
+                   // ApiManager.shared.removeLoader()
+                    //Return if error find
+                    if error != nil {
+                        return
+                    }else{
+                        FirebaseAuth.Auth.auth().signIn(withEmail: data, password: "123123") { [weak self] authResult, error in
+                            guard self != nil else { return }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
