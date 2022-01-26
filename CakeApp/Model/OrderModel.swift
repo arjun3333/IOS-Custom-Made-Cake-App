f5830f749b3828ca4c723310cac003cbba496431
arjun3333 <arjunchowdary823@gmail.com>
2022-01-25 23:46:58 -0500

    orders updated

--- CakeApp/Model/OrderModel.swift
+++ CakeApp/Model/OrderModel.swift
@@ -5,3 +5,28 @@
 //  Created by arjun on 22/01/22.
 //
 
+import Foundation
+
+class OrderModel {
+    var id: String!
+    var image:String!
+    var name: String!
+    var description: String!
+    var price: String!
+    var isDeliver: Bool!
+    var email: String!
+    
+    init(){
+        
+    }
+    
+    init(id: String,image:String,name:String,description:String,price:String,email:String,isDeliver:Bool){
+        self.id = id
+        self.image = image
+        self.name = name
+        self.description = description
+        self.price = price
+        self.isDeliver = isDeliver
+        self.email = email
+    }
+}
