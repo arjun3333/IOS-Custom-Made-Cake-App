80938db8063191d410cc30c7af0a0dcedfa920e0
Shivanee22 <47768137+Shivanee22@users.noreply.github.com>
2022-01-26 19:51:12 -0500

    product modal updated

--- CakeApp/Model/ProductModel.swift
+++ CakeApp/Model/ProductModel.swift
@@ -4,3 +4,28 @@
 //
 //  Created by Shivanee.
 //
+
+import Foundation
+
+
+class ProductModel {
+    var id: String!
+    var image:String!
+    var name: String!
+    var description: String!
+    var price: String!
+    
+    
+    init(){
+        
+    }
+    
+    init(id: String,image:String,name:String,description:String,price:String){
+        self.id = id
+        self.image = image
+        self.name = name
+        self.description = description
+        self.price = price
+    }
+}
+
