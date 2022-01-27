abce6eeae696c0dfe4be71182500781949b1ae62
Anmoldeepsingh0001 <74370241+Anmoldeepsingh0001@users.noreply.github.com>
2022-01-26 20:37:48 -0500

    Update HomeNav.swift
    home nav

--- CakeApp/Scenes/HomeNav.swift
+++ CakeApp/Scenes/HomeNav.swift
@@ -1,1 +1,30 @@
 
+//
+//  HomeNav.swift
+//  CakeApp
+//
+//  Created by anmol
+//
+
+import UIKit
+
+class HomeNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
